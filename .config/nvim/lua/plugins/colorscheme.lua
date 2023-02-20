852fcffb15f4da4342e21aa7811b699c20e564e2
Sudish Joseph <sudish@gmail.com>
2023-02-19 22:34:10 -0500

    Add gruvbox them to LazyVim.

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    --   config = function()
+    --     require("gruvbox").setup({ contrast = "hard" })
+    --   end,
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
