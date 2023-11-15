f911e6b26aee37c60cf0ccc26c48a603c980eced
Sudish Joseph <sudish@gmail.com>
2023-11-14 23:25:42 -0500

    Switch to FantasqueSansM Nerd Font Mono as wezterm font name.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ return {
   color_scheme = "Gruvbox dark, soft (base16)",
   -- font = wezterm.font("MesloLGS NF"),
   -- font = wezterm.font("FiraCode Nerd Font"),
-  font = wezterm.font("FantasqueSansMono Nerd Font"),
+  font = wezterm.font("FantasqueSansM Nerd Font Mono"),
   font_size = 12.0,
   window_decorations = "RESIZE",
 }
