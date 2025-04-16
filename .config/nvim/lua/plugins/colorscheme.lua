642a7c389a4075b747b09919a7fa676353b7af97
Sudish Joseph <sudish@gmail.com>
2025-04-16 16:41:19 -0400

    Whitespace

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
   {
     "ellisonleao/gruvbox.nvim",
-    --   config = function()
-    --     require("gruvbox").setup({ contrast = "hard" })
-    --   end,
+    -- config = function()
+    --   require("gruvbox").setup({ contrast = "hard" })
+    -- end,
   },
 
   {
