bfab418915596ed53a30654d0e6dd637edc00367
Sudish Joseph <sudish@gmail.com>
2025-04-16 16:20:15 -0400

    Tweak some neovim options.

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -7,3 +7,13 @@ vim.g.autoformat = false
 
 -- don't auto continue comments
 vim.cmd([[autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o]])
+
+vim.g.have_nerd_font = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
