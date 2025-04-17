3d3f42cbb5ba83ace31ea85f348a9ba15f08c5b4
Sudish Joseph <sudish@gmail.com>
2025-04-17 16:39:23 -0400

    Change gruvbox color for hl-Whitespace.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,10 @@ return {
     opts = {
       contrast = "", -- can be "hard", "soft" or empty string
       transparent_mode = true,
+      overrides = {
+        -- Tabs, trailing space & nbsp. "#a89984" is brighter
+        Whitespace = { fg = "#7c6f64" }
+      }
     }
   },
 
