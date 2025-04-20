8860793846850874e6b06d159d77867aa190ea37
Sudish Joseph <sudish@gmail.com>
2025-04-20 11:13:55 -0400

    Remove unnecessary nesting in nvim plugin configs.

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,18 +1,16 @@
 -- default LazyVim plugin
 return {
-  {
-    "folke/snacks.nvim",
-    -- opts will be merged with the parent spec
-    opts = {
-      -- a section for each snacks plugin
-      indent = {
-        -- disables all indent guides
-        enabled = false,
-        -- to show only the most relevant indent
-        -- indent = {
-        --   enabled = false,
-        -- },
-      },
+  "folke/snacks.nvim",
+  -- opts will be merged with the parent spec
+  opts = {
+    -- a section for each snacks plugin
+    indent = {
+      -- disable all indent guides
+      enabled = false,
+      -- to show only the most relevant indent
+      -- indent = {
+      --   enabled = false,
+      -- },
     },
   },
 }
