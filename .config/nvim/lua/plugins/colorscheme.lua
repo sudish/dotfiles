99b932074e43bf9eaa85b0855e5fbeb13f5b789a
Sudish Joseph <sudish@gmail.com>
2025-09-03 21:56:49 -0400

    Disable unused nvim themes and zsh aliases.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,29 @@
 return {
-  {
-    "ellisonleao/gruvbox.nvim",
-    opts = {
-      contrast = "", -- can be "hard", "soft" or empty string
-      transparent_mode = true,
-      overrides = {
-        -- NonText must be readable so use lighter gruvbox colors
-        -- NonText = { fg = "#928374" }, -- comment color
-        NonText = { fg = "#7c6f64" }, -- dark4
-        -- Tabs, trailing space & nbsp and other listchars
-        Whitespace = { fg = "#665c54" }, -- dark3
-        -- Missing in gruvbox
-        TreesitterContext = { link = "Folded" },
-        TreesitterContextLineNumber = { link = "CursorLineNr" },
-      },
-    },
-  },
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   opts = {
+  --     contrast = "", -- can be "hard", "soft" or empty string
+  --     transparent_mode = true,
+  --     overrides = {
+  --       -- NonText must be readable so use lighter gruvbox colors
+  --       -- NonText = { fg = "#928374" }, -- comment color
+  --       NonText = { fg = "#7c6f64" }, -- dark4
+  --       -- Tabs, trailing space & nbsp and other listchars
+  --       Whitespace = { fg = "#665c54" }, -- dark3
+  --       -- Missing in gruvbox
+  --       TreesitterContext = { link = "Folded" },
+  --       TreesitterContextLineNumber = { link = "CursorLineNr" },
+  --     },
+  --   },
+  -- },
 
-  {
-    "rebelot/kanagawa.nvim",
-    opts = {
-      keywordStyle = { italic = false },
-      theme = "wave", -- "wave", "dragon", "lotus" (light)
-    },
-  },
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   opts = {
+  --     keywordStyle = { italic = false },
+  --     theme = "wave", -- "wave", "dragon", "lotus" (light)
+  --   },
+  -- },
 
   -- Configure LazyVim to load gruvbox
   -- {
