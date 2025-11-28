b6396145081d22cc41f0a61c1f1f534e91583e2b
Sudish Joseph <sudish@gmail.com>
2025-11-28 08:15:41 -0500

    Disable gist.nvim for now

--- .config/nvim/lua/plugins/gist.lua
+++ .config/nvim/lua/plugins/gist.lua
@@ -1,15 +1,30 @@
 return {
-  {
-    "Rawnly/gist.nvim",
-    cmd = { "GistCreate", "GistCreateFromFile", "GistsList" },
-    config = true
-  },
-  -- `GistsList` opens the selected gist in a terminal buffer,
-  -- nvim-unception uses neovim remote rpc functionality to open the gist in an actual buffer
-  -- and prevents neovim buffer inception
-  {
-    "samjwill/nvim-unception",
-    lazy = false,
-    init = function() vim.g.unception_block_while_host_edits = true end
-  }
+  -- {
+  --   "Rawnly/gist.nvim",
+  --   cmd = { "GistCreate", "GistCreateFromFile", "GistsList" },
+  --   -- config = true,
+  --   opts = {
+  --     platforms = {
+  --       github = {
+  --         list = {
+  --           limit = 300,
+  --           read_only = false,
+  --         },
+  --       },
+  --     },
+  --     list = {
+  --       use_multiplexer = true
+  --     }
+  --   },
+  -- },
+  -- -- `GistsList` opens the selected gist in a terminal buffer,
+  -- -- nvim-unception uses neovim remote rpc functionality to open the gist in an actual buffer
+  -- -- and prevents neovim buffer inception
+  -- {
+  --   "samjwill/nvim-unception",
+  --   lazy = false,
+  --   init = function()
+  --     vim.g.unception_block_while_host_edits = true
+  --   end,
+  -- },
 }
